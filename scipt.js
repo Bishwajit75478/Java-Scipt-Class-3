f084b428e564aa2d7f74421d0d0d0b3d941696b5
bishwajit hazra <bishwajithazra75478@gmail.com>
2022-12-24 12:19:02 +0600

    first commit

---
+++ scipt.js
@@ -0,0 +1,98 @@
+// // 1. comparison operators
+
+// // ==
+
+// console.log(9 == 9);
+// console.log(9 == "9");
+
+// // !=
+// console.log(9 === "9");
+
+// // !=
+// console.log(9 != 9);
+// console.log(9 != 10);
+
+// // !== not equlal value not equlal type
+// console.log(9 !== 9);
+
+// // >
+// console.log(9 > 7);
+
+// // <
+// console.log(9 < 7);
+
+// // >=
+// console.log(9 >= 10);
+
+// // <=
+// console.log(9 <= 8);
+
+// // ?
+
+// // 2.logical operatores
+
+// // 1. &&
+// // 3. ||
+// // 4. !
+
+// // 1. data && date
+// let x = 6;
+// let y = 3;
+// console.log(x < 9);
+// console.log(y > 3);
+// console.log(x < 9 && y > 3);
+
+// // 2. ||
+// console.log(x < 9 || y > 3);
+
+// // !
+// console.log(x == y);
+// console.log(!(x == y));
+
+// 3. Operator precdence
+//  B O D M A S
+// console.log((36 / 6) * 3 + 2 ** 2 - (3 + 5));
+// 6 * 3 + 2 ** 2 - 8
+// 18 + 4 -8
+//  22 - 4
+//    14
+
+// // 4. Template literals
+// // let fristName = "asif";
+// // let lestName = "akbar";
+// // console.log(fristName + " " + lestName);
+
+// const fristName = "Bishwajit";
+// const lestName = "Hazra";
+// let age = 20;
+// let job = "web developer";
+// const country = "Banglasdesh";
+
+// // I'm Bishwajit  Hazra; 20 year old web developer
+// console.log("I'm Bishwajit Hazra; 20 year old web developer!");
+
+// // console.log ("I'm" + " " + fristName + lestName "," + aga + "year old" + job +"!")
+
+// console.log(`I'm ${fristName} ${lestName}; ${age} year old ${job}!`);
+
+// console.log(`I love ${country}`);
+
+// console.log(`frist line
+// second lind
+// therd line
+// fourth line`);
+
+// console.log(`Bangladesh whin ${2 + 1} series`);
+
+// if(){
+
+// }
+// else{
+
+// }
+
+// if (5 > 6) {
+//   console.log("5");
+// } else {
+//   console.log("something else");
+// }
